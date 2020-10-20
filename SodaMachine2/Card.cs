993bd037d24dfbca4dff5f4473043546ceffb6e6
Travis Lawson <travislawclimb@gmail.com>
2020-10-20 11:25:49 -0700

    Set up variables and Constructors for all the classes

---
+++ SodaMachine2/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine2
+{
+    class Card
+    {
+        //Variables (Has a)
+
+        //Constructor (Initial Values)
+        public Card()
+        {
+
+        }
+
+        //Member Methods
+    }
+}
